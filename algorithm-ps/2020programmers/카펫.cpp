b7acdc2d0d50787151518878ac73350d2b31a13f
gwanhyeon <kgh940525@gmail.com>
2020-09-30 17:31:33 +0900

    Add 카펫

--- algorithm-ps/2020programmers/카펫.cpp
+++ algorithm-ps/2020programmers/카펫.cpp
@@ -7,3 +7,41 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+using namespace std;
+int check[100000001];
+int answer[2];
+
+void dfs(int sum, int x, int y){
+    
+    if(x*y == sum){
+        answer[0] = x;
+        answer[1] = y;
+        return;
+    }
+    
+    for(int i=0; i<100000001; i++){
+        
+        if(check[i] == true){
+            continue;
+        }
+        check[i] = true;
+        dfs(sum, x+i, y);
+        check[i] = false;
+        dfs(sum, x, y+i);
+        
+    }
+    
+    
+}
+int main(void){
+    
+    int brown = 10;
+    int yellow = 2;
+    int sum = brown + yellow;
+    
+    dfs(sum,1, 1);
+    cout << answer[0] << answer[1];
+    
+    return 0;
+}
